a8111cb7a7878ca2dcfad47fa0337e05ee33c544
Ezequiel <ezequiel.pir@gmail.com>
2019-10-02 12:07:44 -0300

    Firma de funciones y estructuras

---
+++ lib/funcTrenes.h
@@ -0,0 +1,16 @@
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#define max_est 30
+typedef struct{
+    int ID;
+    int combustible;
+    char modelo[10];
+    char estOrigen[max_est];
+    char estDestino[max_est];
+}TREN;
+void cargarCombustible();
+void solicitarPasoAnden();
+void registrarse();
+void partir();
+void estadoTren();
---
+++ src/funcTrenes.c
@@ -0,0 +1,1 @@
+#include "../lib/funcTrenes.h"
